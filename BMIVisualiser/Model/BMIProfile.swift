5f04cad1a826147e68d687172b0c586ad734f500
Manasi <kmanasi223@gmail.com>
2020-07-13 18:23:21 +0530

    Model created

---
+++ BMIVisualiser/Model/BMIProfile.swift
@@ -0,0 +1,14 @@
+//
+//  BMIProfile.swift
+//  BMIVisualiser
+//
+//  Created by Manasi Kulkarni on 13/07/20.
+//  Copyright © 2020 Manasi Kulkarni. All rights reserved.
+//
+
+import Foundation
+
+struct BMIProfile {
+    var name : String?
+    var dob : Date?
+}
